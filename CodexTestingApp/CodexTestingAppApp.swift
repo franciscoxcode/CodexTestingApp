e908e36d5bc03362806726b256f27dc432ef45d9
Francisco Casillas <chiccasillas@gmail.com>
2025-09-15 12:00:32 -0600

    Initial Commit

---
+++ CodexTestingApp/CodexTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexTestingAppApp.swift
+//  CodexTestingApp
+//
+//  Created by Francisco Jean on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CodexTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
